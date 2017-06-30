9e187e4b695aecffa0d7ca8437601d4592fd18a9
pesebastian <paolosebastian21@gmail.com>
2017-06-30 14:07:02 -0700

    Arduino Update 6/30/2017
    Created headers for positive/negative linear regulators as well as the digital pots.

---
+++ arduino_main/DigitalPot.h
@@ -0,0 +1,18 @@
+#ifndef DigitalPot_h
+#define DigitalPot_h
+
+#include "Arduino.h"
+
+class DigitalPot {
+  public:
+  DigitalPot(int chipSelect);
+  void setRes1(double res1);
+  void setRes2(double res2);
+  void writeValues();
+  
+  private:
+  double res1;
+  double res2;
+};
+
+#endif
