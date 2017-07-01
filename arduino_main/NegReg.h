20a2e57d8684e5b3a73a4ede9e9a7353c5416111
pesebastian <paolosebastian21@gmail.com>
2017-07-01 15:11:05 -0700

    Arduino Update 7/1/2017
    Updated for Andrew.

---
+++ arduino_main/NegReg.h
@@ -0,0 +1,23 @@
+#ifndef NegReg_h
+#define NegReg_h
+#define SHDN1 ___
+#define SHDN2 ___
+
+#include "Arduino.h"
+#include "DigitalPot.h"
+
+class NegReg {
+  public:
+  NegReg(double volt1, double volt2 = 0);
+  void setV1(double volt1);
+  void setV2(double volt2);
+  void reset();
+  void calculatePotValue(double volt1, double volt2); 
+  
+  private:
+  double v1;
+  double v2;
+  DigitalPot negPot;
+};
+
+#endif
