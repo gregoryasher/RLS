4cbe68287d91a046cad834fef02dd1861e71b3ea
pesebastian <paolosebastian21@gmail.com>
2017-07-07 14:49:15 -0700

    Arduino Update 7/7/2017
    Created and commented negative and positive regulator classes as well as the digital potentiometer class.

---
+++ arduino_main/PosReg.cpp
@@ -0,0 +1,91 @@
+/******************************************************************************
+  PosReg.cpp
+  This implementation file contains implementation code for the positive 
+  linear regulators.
+
+  Created by Paolo Sebastian, July 2017
+  Reseased into the public domain.
+******************************************************************************/
+
+#include "Arduino.h"
+#include "PosReg.h"
+#include "DigitalPot.h"
+
+/*********************************************
+   Constructor for positive linear regulators.
+   Sets the positive digital potentiometer pin  
+   to the appropriate chip enable.    
+**********************************************/
+PosReg::PosReg(double volt1, double volt2 = 0.0) {
+  v1 = volt1;
+  v2 = volt2;
+  posPot = new DigitalPot(POS_CS);
+}
+
+/*********************************************
+   Mutator for requested voltage input 1.  
+**********************************************/
+void PosReg::setV1(double volt1) {
+  v1 = volt1;
+}
+
+/*********************************************
+   Mutator for requested voltage input 2.  
+**********************************************/
+void PosReg::setV2(double volt2) {
+  v2 = volt2;
+}
+
+/*********************************************
+   Takes in requested voltage values and 
+   converts them into the appropriate 
+   integer potentiometer resistance values.
+   It then sets the potentiometer to the requested
+   resistance(s).
+**********************************************/
+void PosReg::calculatePotValue(double volt1, double volt2) {
+
+  //calculates pot values as an integer
+  int posDigRes1 = posPotCalc(volt1);
+  int posDigRes2 = posPotCalc(volt2);
+
+  //shuts down associated regulator if 
+  //user does not request a voltage
+  if ((posDigRes1) == 0) {
+    digitalWrite(V1_PLUS_SHDN, LOW);
+  }
+  else {
+    posPot.setRes1(posDigRes1);
+  }
+
+  if ((posDigRes2) == 0) {
+    digitalWrite(V2_PLUS_SHDN, LOW);
+  }
+  else {
+    posPot.setRes2(posDigRes2);
+  }
+
+  //writes resistor values to the potentiomter 
+  posPot.writeValues(V1_PLUS_SHDN, V2_PLUS_SHDN);
+}
+
+/*********************************************
+   Computes the required digital potentiometer
+   resitance and returns the value as an 
+   integer.
+**********************************************/
+int PosReg::posPotCalc(double vin){  
+  int posPotRes;
+
+  //calculation to determine potentiometer 
+  //resistance for the particular positive regulator
+  if (vin > 1.21) {
+    posPotRes = ((vin - 1.21) / ((1.21 / 1780.0) + 0.000003));
+  }
+  else {
+    posPotRes = 0;
+  }
+  
+  return posPotRes;
+}
+
--- arduino_main/PosReg.h
+++ arduino_main/PosReg.h
@@ -1,23 +1,51 @@
+/******************************************************************************
+  PosReg.h
+  This header file contains code to control the positive set of linear 
+  regulators based on a daughterboard requested input voltage.
+  
+  Created by Paolo Sebastian, July 2017
+  Reseased into the public domain.
+******************************************************************************/
+
 #ifndef PosReg_h
 #define PosReg_h
-#define SHDN1 ___
-#define SHDN2 ___
 
 #include "Arduino.h"
 #include "DigitalPot.h"
 
 class PosReg {
   public:
-  PosReg(double volt1, double volt2 = 0);
+
+  //defaults second positive regulator to 0.0 if daughterboard does not
+  //request an input voltage for it
+  PosReg(double volt1, double volt2 = 0.0);
+  
   void setV1(double volt1);
   void setV2(double volt2);
-  void reset();
+
+  //calculates required digital potentiometer values for the requested
+  //input voltages and sets the digital potentiometer to calculated value 
   void calculatePotValue(double volt1, double volt2); 
+
+  //helper function that calculates the digitial potentiometer resistance
+  //as an int
+  int posPotCalc(double vin); 
   
   private:
+
+  //requested voltages
   double v1;
   double v2;
+
+  //digital potentiometer associated with the positive regulators
   DigitalPot posPot;
+
+  //appropriate pin allocations for the shutdown pins of both 
+  //positive linear regulators as well as the chip enable for the 
+  //positive digital potentiometer attached to them 
+  const byte V1_PLUS_SHDN = A8;
+  const byte V2_PLUS_SHDN = A9;
+  const byte POS_CS = 50;
 };
 
 #endif
